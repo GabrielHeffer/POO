d268d311cf8f9fe2398554b39c3e7f3ec0938d1f
heffis <heffergabriel9@gmail.com>
2019-10-18 20:11:44 -0300

    Nova classe para registrar nome dos jogadores

--- gui/PNjogo.java
+++ gui/PNjogo.java
@@ -1,10 +1,11 @@
 package gui;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PNjogo extends JFrame {
+public class PNjogo extends JFrame implements ActionListener {
 
-    JPanel IDjog;
     public PNjogo(){
         Toolkit tk=Toolkit.getDefaultToolkit();
         Dimension screenSize=tk.getScreenSize();
@@ -17,32 +18,17 @@ public class PNjogo extends JFrame {
         IdJogadoresPanel();
 
     }
-    private void IdJogadoresPanel(){
-        IDjog = new JPanel();
-        JLabel jog1Label = new JLabel("Jogador 1 : ");
-        JLabel jog2Label = new JLabel("Jogador 2 : ");
-        JTextField TextFieldjog1 = new JTextField(15);
-        JTextField TextFieldjog2 = new JTextField(15);
-        JButton ok = new JButton("OK");
-        Dimension textjog = jog1Label.getPreferredSize();
-        Dimension okBt = ok.getPreferredSize();
-        IDjog.setLayout(null);
-        jog1Label.setBounds(40,50,textjog.width,textjog.height);
-        jog2Label.setBounds(40,120,textjog.width,textjog.height);
-        TextFieldjog1.setBounds(120,40,200,textjog.height+20);
-        TextFieldjog2.setBounds(120,110,200,textjog.height+20);
-        ok.setBounds(200 - okBt.width/2 - 10,200,okBt.width,okBt.height);
-        IDjog.add(jog1Label);
-        IDjog.add(TextFieldjog1);
-        IDjog.add(jog2Label);
-        IDjog.add(TextFieldjog2);
-        IDjog.add(ok);
-        getContentPane().add(IDjog);
+    private void IdJogadoresPanel (){
+        getContentPane().add(new PNIndetJog());
     }
 
     public static void main(String args[]){
         PNjogo x = new PNjogo();
         x.setTitle("Jogadores");
         x.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
